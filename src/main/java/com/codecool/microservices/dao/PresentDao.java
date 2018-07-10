efd39955988df252e7c28e38a04c6b658b6ba1d6
Adrienn <adrienn.d91@gmail.com>
2018-07-10 15:16:57 +0200

    basics accomplished

--- src/main/java/com/codecool/microservices/dao/PresentDao.java
+++ src/main/java/com/codecool/microservices/dao/PresentDao.java
@@ -1,4 +1,34 @@
 package com.codecool.microservices.dao;
 
+import com.codecool.microservices.model.Present;
+import com.codecool.microservices.utility.JsonUtil;
+import com.codecool.microservices.utility.UrlParser;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PresentDao {
+
+    @Autowired
+    private UrlParser urlParser;
+
+    @Autowired
+    private JsonUtil jsonUtil;
+
+    private JSONObject presentJSON;
+
+    private void getPresentJson(){
+        presentJSON = ???.getJson(urlParser.getPresentRoute());
+    }
+
+    private Present makePresentFromJson(){
+        Integer id = Integer.valueOf(presentJSON.get("id").toString());
+        String name = presentJSON.get("name").toString();
+        double price = Double.valueOf(presentJSON.get("price").toString());
+        String category = presentJSON.get("category").toString();
+        boolean available = Boolean.valueOf(presentJSON.get("available").toString());
+        Integer ownerId = Integer.valueOf(presentJSON.get("ownerid").toString());
+        return new Present(id, name, price, category, available, ownerId);
+    }
 }
--- src/main/java/com/codecool/microservices/model/Present.java
+++ src/main/java/com/codecool/microservices/model/Present.java
@@ -1,4 +1,45 @@
 package com.codecool.microservices.model;
 
 public class Present {
+
+    private int id;
+    private String name;
+    private double price;
+
+    private String category;
+    private boolean available;
+    private int ownerId;
+
+    public Present(int id, String name, double price, String category, boolean available, int ownerId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.available = available;
+        this.ownerId = ownerId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
 }
