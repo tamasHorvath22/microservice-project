8616e1f0d1f6af8b41293b6da3cf7ce2b8aff526
Tamas Horvath <horvathtamas22@gmail.com>
2018-07-10 15:23:30 +0200

    get json from url method

--- src/main/java/com/codecool/microservices/dao/WalletDao.java
+++ src/main/java/com/codecool/microservices/dao/WalletDao.java
@@ -1,4 +1,33 @@
 package com.codecool.microservices.dao;
 
+import com.codecool.microservices.model.Wallet;
+import com.codecool.microservices.utility.UrlParser;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class WalletDao {
+
+    private JSONObject walletJson;
+    private Wallet wallet;
+
+    @Autowired
+    private UrlParser urlParser;
+
+    @Autowired
+    private Util util;
+
+    public Wallet getWallet() {
+        getJson();
+        createWalletObject();
+        return wallet;
+    }
+
+    private void getJson() {
+        walletJson = util.getJson(urlParser.getEwalletRoute());
+    }
+
+    private void createWalletObject() {
+        wallet = new Wallet(Long.valueOf(walletJson.get("userId").toString()),
+                            Double.valueOf(walletJson.get("balance").toString()));
+    }
 }
--- src/main/java/com/codecool/microservices/utility/JsonUtil.java
+++ src/main/java/com/codecool/microservices/utility/JsonUtil.java
@@ -1,10 +1,14 @@
 package com.codecool.microservices.utility;
 
+import org.json.JSONException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Iterator;
 
 @Component
@@ -20,5 +24,26 @@ public class JsonUtil {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public org.json.JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            org.json.JSONObject json = new org.json.JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 }
