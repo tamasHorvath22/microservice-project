de8a3d68a1c153689428dd40725ef305e3088518
Tamas Horvath <horvathtamas22@gmail.com>
2018-07-10 14:55:01 +0200

    wallet done, util needs to specify when it is done

--- src/main/java/com/codecool/microservices/dao/WalletDao.java
+++ src/main/java/com/codecool/microservices/dao/WalletDao.java
@@ -1,4 +1,28 @@
 package com.codecool.microservices.dao;
 
+import com.codecool.microservices.model.Wallet;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class WalletDao {
+
+    private JSONObject walletJson;
+    private Wallet wallet;
+
+    @Autowired
+    private JsonUtil jsonUtil;
+
+    public Wallet getWallet() {
+        getJson();
+        createWalletObject();
+        return wallet;
+    }
+
+    private void getJson() {
+        walletJson = methodThatReturnsJson(jsonUtil.getWalletRoute);
+    }
+
+    private void createWalletObject() {
+        wallet = new Wallet(walletJson.get("userId"), walletJson.get("balance"))
+    }
 }
--- src/main/java/com/codecool/microservices/model/Wallet.java
+++ src/main/java/com/codecool/microservices/model/Wallet.java
@@ -1,4 +1,25 @@
 package com.codecool.microservices.model;
 
+
 public class Wallet {
+
+    private long id;
+    private double balance;
+
+    public Wallet(long id, double balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
