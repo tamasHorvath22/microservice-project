2ead95fec37cbc70aed64c4f319cdc3d3c9da644
Laszlo Jeszmas <rolne1992@gmail.com>
2018-07-11 15:34:14 +0200

    List of Presents is get by the presents-microservice.

--- src/main/java/com/codecool/microservices/dao/PresentDao.java
+++ src/main/java/com/codecool/microservices/dao/PresentDao.java
@@ -3,6 +3,7 @@ package com.codecool.microservices.dao;
 import com.codecool.microservices.model.Present;
 import com.codecool.microservices.utility.JsonUtil;
 import com.codecool.microservices.utility.UrlParser;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,9 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class PresentDao {
@@ -48,5 +51,29 @@ public class PresentDao {
     public Present getPresent(String route) throws ParseException{
         getPresentJson(route);
         return makePresentFromJson();
+    }
+
+    public List<Present> getAllPresentById(long id){
+        List<Present> presents = new ArrayList<>();
+        DateFormat format = new SimpleDateFormat("YYYY-mm-dd");
+        try{
+            String route = urlParser.getUserRoute() + "user/" + id;
+            JSONObject response = jsonUtil.readJsonFromUrl(route );
+            JSONArray jsonArray = (JSONArray)response.get("presents");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject json = (JSONObject) jsonArray.get(i);
+                Present present = new Present(json.getInt("id"),
+                        json.getString("name"),
+                        json.getDouble("price"),
+                        json.getString("category"),
+                        json.getBoolean("available"),
+                        json.getInt("userId"),
+                        format.parse(json.getString("creation")));
+                presents.add(present);
+            }
+        } catch (IOException | ParseException ex){
+            ex.printStackTrace();
+        }
+        return presents;
     }
 }
--- src/main/java/com/codecool/microservices/service/PresentService.java
+++ src/main/java/com/codecool/microservices/service/PresentService.java
@@ -1,4 +1,19 @@
 package com.codecool.microservices.service;
 
+import com.codecool.microservices.dao.PresentDao;
+import com.codecool.microservices.model.Present;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PresentService {
+
+    @Autowired
+    private PresentDao presentDao;
+
+    public List<Present> getPresentsByUserId(long id){
+        return presentDao.getAllPresentById(id);
+    }
 }
