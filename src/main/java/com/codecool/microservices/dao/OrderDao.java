a1ddba8a1c153b1fcf1fada37d04e646c2fde183
Ben85 <outdoorsport85@gmail.com>
2018-07-11 14:19:33 +0200

    OrderDao getAllOrders

--- src/main/java/com/codecool/microservices/dao/OrderDao.java
+++ src/main/java/com/codecool/microservices/dao/OrderDao.java
@@ -1,4 +1,28 @@
 package com.codecool.microservices.dao;
 
+import com.codecool.microservices.model.Order;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class OrderDao {
+
+
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    public List<Order> getAllOrders() {
+        final String uri = "http://localhost:8080/present";
+        List<Order> orders = new ArrayList<>();
+        try {
+            orders = Arrays.asList(restTemplate.getForObject(uri, Order[].class));
+        } catch (ResourceAccessException e) {
+            System.out.println("Order microservice unavailable");
+        }
+        System.out.println(orders.toString());
+        return orders;
+    }
+
 }
