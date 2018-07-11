eecaab9df53f733d6b0dc89ae8175ea504205aaf
Tamás Varjas <tamas.varjas00@gmail.com>
2018-07-11 13:42:00 +0200

    add to cart

--- src/main/java/com/codecool/microservices/controller/CartController.java
+++ src/main/java/com/codecool/microservices/controller/CartController.java
@@ -1,4 +1,25 @@
 package com.codecool.microservices.controller;
 
+import com.codecool.microservices.model.User;
+import com.codecool.microservices.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
 public class CartController {
+
+    private final static String MAIN_PAGE = "index";
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add-to-cart")
+    public String addToCart(@RequestParam("presentId") long presentId, @SessionAttribute("user") User user) {
+        long userId = user.getId();
+        cartService.add(userId, presentId);
+        return MAIN_PAGE;
+    }
 }
--- src/main/java/com/codecool/microservices/service/CartService.java
+++ src/main/java/com/codecool/microservices/service/CartService.java
@@ -1,4 +1,10 @@
 package com.codecool.microservices.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartService {
+
+    public void add(long userId, long presentId) {
+    }
 }
