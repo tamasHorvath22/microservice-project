b772b8b6b3f6614cff1bd25c72a00d5161134dcd
Unknown <lisztes.alex@gmail.com>
2018-07-13 01:13:44 +0200

    Adding new presents from request

--- src/main/java/com/codecool/microservices/controller/PresentController.java
+++ src/main/java/com/codecool/microservices/controller/PresentController.java
@@ -4,14 +4,13 @@ import com.codecool.microservices.model.Present;
 import com.codecool.microservices.model.User;
 import com.codecool.microservices.service.PresentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
-@RestController
+@Controller
 public class PresentController {
 
     @Autowired
@@ -20,5 +19,24 @@ public class PresentController {
     @GetMapping(value = "/getPresents")
     public List<Present> getAllPresentByUserId(@SessionAttribute("user") User user){
         return presentService.getPresentsByUserId(user.getId());
+    }
+
+    @GetMapping("/addNewPresent")
+    public String renderForm() {
+        return "presentForm";
+    }
+
+    @PostMapping("addNewPresent")
+    public String addNewPresent(
+            @SessionAttribute User user,
+            @RequestParam String name,
+            @RequestParam String description,
+            @RequestParam double price,
+            @RequestParam String category,
+            @RequestParam String image
+    ) {
+        Present newPresent = new Present(name, description, price, category, true, (int) user.getId(), image, new Date());
+        presentService.addPresent(newPresent);
+        return "redirect:/";
     }
 }
