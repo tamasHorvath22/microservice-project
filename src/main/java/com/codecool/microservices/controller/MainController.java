4abef285f5611be1140437c90b1d2a0e00339c63
Adrienn <adrienn.d91@gmail.com>
2018-07-12 18:23:07 +0200

    login redirect

--- src/main/java/com/codecool/microservices/controller/MainController.java
+++ src/main/java/com/codecool/microservices/controller/MainController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.support.SessionStatus;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,19 +22,28 @@ public class MainController {
     @Autowired
     private PresentService presentService;
 
+    @Autowired
+    private RestSignatureFilter restSignatureFilter;
+
     @GetMapping({"", "index"})
-    public String indexPage(@SessionAttribute("user") User user, Model model){
+    public String indexPage(@SessionAttribute("user") User user, Model model, HttpServletRequest request){
         List<Present> presents = new ArrayList<>();
         List<Present> randomPresents = new ArrayList<>();
-        try{
-            presents = presentService.getAllPresents();
-            randomPresents = presentService.getFourRandomPresents();
-        } catch (ParseException ex) {
-            System.out.println("no presents :(");
+        if(user.getId() != 0L) {
+            try {
+                presents = presentService.getAllPresents();
+                randomPresents = presentService.getFourRandomPresents();
+            } catch (ParseException ex) {
+                System.out.println("no presents :(");
+            }
+            model.addAttribute("presents", presents);
+            model.addAttribute("user", user);
+            model.addAttribute("randomPresents", randomPresents);
+            return "index";
+        } else {
+            HttpSession session =request.getSession(false);
+            session.invalidate();
+            return "redirect:/login";
         }
-        model.addAttribute("presents", presents);
-        model.addAttribute("user", user);
-        model.addAttribute("randomPresents", randomPresents);
-        return "index";
     }
 }
