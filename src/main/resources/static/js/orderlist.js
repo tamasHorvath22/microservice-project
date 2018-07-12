07754233fceb97d52f49384b638943f2c03fc573
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-07-12 17:29:14 +0200

    Fix the order menu on the profile page

--- src/main/resources/static/js/orderlist.js
+++ src/main/resources/static/js/orderlist.js
@@ -27,19 +27,21 @@ orderdom = {
             actualOrder.innerHTML = "<div><h3>Order " + orders[i].id +
                 " Ordered time: " + orders[i].timestamp.toString() + "</h3></div>";
             for (let order of orders[i].order) {
-                actualPresents.innerHTML += "<div id=present-wish-" + order.id + " class=\"row mb-5\">\n" +
-                    "<div class=\"col-xl-12\">\n" +
-                    "<div class=\"media-boxes\">\n" +
-                    "<div class=\"media\">\n" +
-                    "<div class=\"media-body tm-bg-gray\">\n" +
-                    "<img src=" + order.imageUrl + " alt=\"Image\" class='mr-3 present-img'>\n" +
-                    "<div class=\"tm-description-box\">\n" +
-                    "<h5 class=\"tm-text-blue\">" + order.name + "</h5>\n" +
-                    "<p class=\"mb-0 description\">" + order.description + "</p>\n" +
-                    "</div>\n" +
-                    "<div class=\"tm-buy-box\">\n" +
-                    "<span class=\"tm-text-blue tm-price-tag\">" + order.price + " Codecoin</span>\n" +
-                    "</div></div></div></div></div></div>";
+                if (order.id > 0) {
+                    actualPresents.innerHTML += "<div id=present-wish-" + order.id + " class=\"row mb-5\">\n" +
+                        "<div class=\"col-xl-12\">\n" +
+                        "<div class=\"media-boxes\">\n" +
+                        "<div class=\"media\">\n" +
+                        "<div class=\"media-body tm-bg-gray\">\n" +
+                        "<img src='" + order.imageUrl + "' alt=\"Image\" class='mr-3 present-img'>\n" +
+                        "<div class=\"tm-description-box\">\n" +
+                        "<h5 class=\"tm-text-blue\">" + order.name + "</h5>\n" +
+                        "<p class=\"mb-0 description\">" + order.description + "</p>\n" +
+                        "</div>\n" +
+                        "<div class=\"tm-buy-box\">\n" +
+                        "<span class=\"tm-text-blue tm-price-tag\">" + order.price + " Codecoin</span>\n" +
+                        "</div></div></div></div></div></div>";
+                }
             }
         }
 
