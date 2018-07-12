4cb4626bf12b0ff13eb9d877e4641c84607e724f
Tamas Horvath <horvathtamas22@gmail.com>
2018-07-12 16:36:14 +0200

    comments removed

--- src/main/java/com/codecool/microservices/controller/PaymentController.java
+++ src/main/java/com/codecool/microservices/controller/PaymentController.java
@@ -2,12 +2,10 @@ package com.codecool.microservices.controller;
 
 import com.codecool.microservices.dao.CommunicationDao;
 import com.codecool.microservices.dao.PresentDao;
-import com.codecool.microservices.model.Order;
 import com.codecool.microservices.model.Present;
 import com.codecool.microservices.model.User;
 import com.codecool.microservices.service.CartService;
 import com.codecool.microservices.service.OrderService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -47,31 +44,5 @@ public class PaymentController {
         model.addAttribute("presentList", presentList);
         model.addAttribute("sumPrice", sumPrice);
         return "payment";
-    }
-
-//    @GetMapping(value = "/test")
-//    public String cucc() {
-//        Date date = new Date();
-////        Order order = new Order(1, 1, date);
-//        Present present1 = new Present(1L, "name1", "desc", 1.1, "str", true, 2, "im", date);
-//        Present present2 = new Present(2L, "name2", "desc", 1.1, "str", true, 3, "im", date);
-//
-//        List<Present> presentList = new ArrayList<>();
-//        presentList.add(present1);
-//        presentList.add(present2);
-////        order.setOrder(presentList);
-//        User buyer = new User(1L, "Buyer", "München", "horvathtamas22@gmail.com", "ff", "Rr");
-//        User seller1 = new User(2L, "First", "Second", "kondacspeter@gmail.com", "ff", "Rr");
-//        User seller2 = new User(3L, "First", "Second", "horvathtamas22@gmail.com", "ff", "Rr");
-//
-//        List<User> sellerList = new ArrayList<>();
-//        sellerList.add(seller1);
-//        sellerList.add(seller2);
-//
-//
-////        CommunicationDao communicationDao = new CommunicationDao();
-////        communicationDao.sendPurchaseMail(user, order);
-//        communicationDao.sendSoldEmail(buyer, sellerList, presentList);
-//        return "about";
     }
 }
