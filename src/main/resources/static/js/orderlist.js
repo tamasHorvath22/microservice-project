6c4205b3b1c29ccd1a51b6208b64e03ee9587592
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-07-12 16:18:44 +0200

    Maybe finished the orders on profile page

--- src/main/resources/static/js/orderlist.js
+++ src/main/resources/static/js/orderlist.js
@@ -24,9 +24,22 @@ orderdom = {
             let actualOrderWrapper = document.getElementsByClassName("order-wrapper" + i)[0];
             let actualOrder = actualOrderWrapper.getElementsByClassName("order")[0];
             let actualPresents = actualOrderWrapper.getElementsByClassName("presents")[0];
-            actualOrder.innerHTML = "<div>" + orders[i].id + "</div>";
-            for (let presentid of orders[i].presentIds) {
-                actualPresents.innerHTML += "<div>" + presentid + "</div>";
+            actualOrder.innerHTML = "<div><h3>Order " + orders[i].id +
+                " Ordered time: " + orders[i].timestamp.toString() + "</h3></div>";
+            for (let order of orders[i].order) {
+                actualPresents.innerHTML += "<div id=present-wish-" + order.id + " class=\"row mb-5\">\n" +
+                    "<div class=\"col-xl-12\">\n" +
+                    "<div class=\"media-boxes\">\n" +
+                    "<div class=\"media\">\n" +
+                    "<div class=\"media-body tm-bg-gray\">\n" +
+                    "<img src=" + order.imageUrl + " alt=\"Image\" class=\"mr-3\">\n" +
+                    "<div class=\"tm-description-box\">\n" +
+                    "<h5 class=\"tm-text-blue\">" + order.name + "</h5>\n" +
+                    "<p class=\"mb-0 description\">" + order.description + "</p>\n" +
+                    "</div>\n" +
+                    "<div class=\"tm-buy-box\">\n" +
+                    "<span class=\"tm-text-blue tm-price-tag\">" + order.price + " Codecoin</span>\n" +
+                    "</div></div></div></div></div></div>";
             }
         }
 
